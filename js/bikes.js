aec599420ad1110e6d2b87d3dc9397706fa8bc17
Hongyi Tao <taohongyi.toby@gmail.com>
2020-02-12 20:49:01 +1000

    add bike spec taps

--- js/bikes.js
+++ js/bikes.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
       $(this).addClass('active');
   });
 
+
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
 });
